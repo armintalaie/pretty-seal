dad73f8e691006c43dd31c6a19d5364c498626fc
armintalaie <talaiearmin78@gmail.com>
2022-09-05 01:15:52 -0700

    QR code to join rooms

--- src/components/invites/invite.tsx
+++ src/components/invites/invite.tsx
@@ -1,21 +1,27 @@
+import { useContext } from "react";
 import QRCode from "react-qr-code";
+import { SpaceContext } from "../../context/spaceContext";
 import { API_BASE_URL } from "../../services/apiHandler";
 import Info from "../common/info/info";
 import "./index.css";
 
 export default function Invite({ roomId }: { roomId: string }) {
+  const { spaceInfo } = useContext(SpaceContext);
   return (
     <div>
       <h2>Invite</h2>
       <section className="invite-code">
-        {/* <div>
-          <QRCode size={150} value={`${API_BASE_URL}/roomId?${roomId}`} />
-        </div> */}
+        <div>
+          <QRCode
+            size={150}
+            value={`${API_BASE_URL}/spaces/${spaceInfo?.domainId}/rooms/${roomId}`}
+          />
+        </div>
       </section>
       <Info>
         <div>
           <h4>You can share the QR code that will prompt others to join this room</h4>
-          <h5>{roomId}</h5>
+          <h6>{roomId}</h6>
         </div>
       </Info>
     </div>
