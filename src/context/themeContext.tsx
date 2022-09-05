528ef15fa6c353e9ac9071c4c13b3beb25aeb461
armintalaie <talaiearmin78@gmail.com>
2022-09-05 01:17:17 -0700

    theme update

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -5,7 +5,7 @@ interface ThemeContextInterface {
   changeTheme: (theme?: ThemeDetail) => void;
 }
 
-const defaultTheme = defaultThemes.sealyBlue;
+const defaultTheme = defaultThemes.sealyLight;
 export const ThemeContext = createContext<ThemeContextInterface>({
   currentTheme: defaultTheme,
   changeTheme: () => {},
